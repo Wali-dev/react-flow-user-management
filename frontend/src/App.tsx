285c3d99df88734dfd16cb3cb30093468b284e75
Wali Cornell <cornelhassan780@gmail.com>
2025-01-22 18:41:55 +0530

    frontend setup complete

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-blue-500'>Is this the reak life?</h1>
+      <button className="btn btn-neutral">Button</button>
+    </>
+  )
+}
+
+export default App
